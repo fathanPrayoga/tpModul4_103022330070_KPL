5693fc67a34da13c772e8aada9a65ca41b0b7b8f
fathanPrayoga <fathan.wicaksono@gmail.com>
2025-03-10 13:10:50 +0700

    menambahkan implementasi dengan table-driven

--- tpModul4_103022330070(2)/Program.cs
+++ tpModul4_103022330070(2)/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class kodePos
+{
+    private static Dictionary<String, int> tabelKodePos = new Dictionary<String, int>
+   {
+        {"Batununggal", 40266},
+        {"A. Kujangsari", 40287},
+        {"Mengger", 40267},
+        {"Wates", 40256},
+        {"Cijaura", 40287},
+        {"Jatisari", 40286},
+        {"Margasari", 40286},
+        {"Sekejati", 40286},
+        {"Kebonwaru", 40272},
+        {"Maleer", 40274},
+        {"Samoja", 40273}
+   };
+
+    public static int? GetKodePos(string kelurahan)
+    {
+        if (tabelKodePos.TryGetValue(kelurahan, out int kodePos))
+        {
+            return kodePos;
+        }
+        return null;
+    }
+}
